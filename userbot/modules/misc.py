9e6dc94630fbf050c346278eb35ae600c7d1c0cd
Avinash Reddy <cassilasreddythemostwanted3108@gmail.com>
2019-08-27 21:24:06 +0530

    Update misc.py

--- userbot/modules/misc.py
+++ userbot/modules/misc.py
@@ -144,19 +144,14 @@ async def repo_is_here(wannasee):
 @errors_handler
 async def json(event):
     if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
-        if event.fwd_from:
-            return
         the_real_message = None
         reply_to_id = None
         if event.reply_to_msg_id:
             previous_message = await event.get_reply_message()
-            #the_real_message = previous_message.stringify()
-            the_real_message = json.dumps(previous_message, 
-                                          sort_keys = True, indent = 4)
+            the_real_message = previous_message.stringify()
             reply_to_id = event.reply_to_msg_id
         else:
-            the_real_message = json.dumps(event, 
-                                          sort_keys = True, indent = 4)
+            the_real_message = event.stringify()
             reply_to_id = event.message.id
             
         with io.BytesIO(str.encode(the_real_message)) as out_file:
