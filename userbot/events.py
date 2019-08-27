56a833537501e566a972c4810cb0705ef7ff4af0
Avinash Reddy <cassilasreddythemostwanted3108@gmail.com>
2019-08-27 20:27:42 +0530

    Update events.py

--- userbot/events.py
+++ userbot/events.py
@@ -51,17 +51,19 @@ def errors_handler(func):
                 'date': datetime.datetime.now()
             }
 
-            text = "**Sorry, I encountered a error!**\n"
+            text = "**USERBOT CRASH REPORT**\n\n"
+           
             link = "[here](https://t.me/PaperplaneExtendedSupport)"
             text += "If you wanna you can report it"
             text += f"- just forward this message {link}.\n"
-            text += "I won't log anything except the fact of error and date\n"
+            text += "Nothing is logged except the fact of error and date\n"
 
             ftext = "\nDisclaimer:\nThis file uploaded ONLY here,"
             ftext += "\nwe logged only fact of error and date,"
             ftext += "\nwe respect your privacy,"
             ftext += "\nyou may not report this error if you've"
             ftext += "\nany confidential data here, no one will see your data\n\n"
+            
             ftext += "--------BEGIN USERBOT TRACEBACK LOG--------"
             ftext += "\nDate: " + date
             ftext += "\nGroup ID: " + str(errors.chat_id)
